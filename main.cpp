51f0e6e374aaa5d3f95179a2e897ca015aba6df0
T.J. Tarazevits <tarazevits@gmail.com>
2017-07-07 22:11:07 -0400

    Basic Prime Number Comparison between C++ and JS

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <nan.h>
 
-NAN_METHOD(Hello) {
-  auto message = Nan::New("Hello from C++!").ToLocalChecked();
+NAN_METHOD(IsPrime) {
+  if (!info[0]->IsNumber()) {
+    Nan::ThrowTypeError("argument must be a number!");
+    return;
+  }
 
-  info.GetReturnValue().Set(message);
+  int number = (int) info[0]->NumberValue();
+
+  if (number < 2) {
+    info.GetReturnValue().Set(Nan::False());
+    return;
+  }
+
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0) {
+      info.GetReturnValue().Set(Nan::False());
+      return;
+    }
+  }
+
+  info.GetReturnValue().Set(Nan::True());
 }
 
 NAN_MODULE_INIT(Initialize) {
-  NAN_EXPORT(target, Hello);
+  NAN_EXPORT(target, IsPrime);
 }
 
 NODE_MODULE(addon, Initialize);
--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-const { Hello } = require('./build/Release/addon');
+const { IsPrime } = require('./build/Release/addon');
+const isPrime = require('./isPrime');
 
-console.log(Hello());
+const number = 654188429;
+const NATIVE = 'native';
+const JS = 'js';
+
+for (let i = 0; i < 10; i++) {
+  console.time(NATIVE);
+  console.log(`${NATIVE}: checking whether ${number} is prime... ${IsPrime(number)}`);
+  console.timeEnd(NATIVE);
+  console.log('');
+}
+console.log("done with native tests");
+
+for (let i = 0; i < 10; i++) {
+  console.time(JS);
+  console.log(`${JS}: checking whether ${number} is prime... ${isPrime(number)}`);
+  console.timeEnd(JS);
+  console.log('');
+}
+console.log("done with all tests");
